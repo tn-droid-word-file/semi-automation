84b8f74032153a3928e58439837126ec839a17b3
Wig Cheng <wig.cheng@technexion.com>
2019-10-22 15:41:18 +0800

    auto_test: first initialization
    Not ready

---
+++ auto_test.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+LCD_W=800
+LCD_H=480
+
+HDMI_1080P_W=1920
+HDMI_1080P_H=1080
+
+CURRENT_RES=$(adb shell dumpsys window | grep cur= |tr -s " " | cut -d " " -f 4|cut -d "=" -f 2)
+CURRENT_W=$(echo "$CURRENT_RES" | awk -Fx '{print $1}')
+CURRENT_H=$(echo "$CURRENT_RES" | awk -Fx '{print $2}')
+
+
+if [[ "$CURRENT_W" -eq 800 ]] && [[ "$CURRENT_H" -eq 480 ]];then
+#SWIPE_X1=$(echo | awk "{print 288*"$X_SCALE"}")
+SWIPE_X1=288
+SWIPE_X2=288
+SWIPE_Y1=240
+SWIPE_Y2=0
+BT_TAP_X=288
+BT_TAP_Y=240
+CAM_CAPTURE_X=740
+CAM_CAPTURE_Y=216
+CAM_BACK_X=737
+CAM_BACK_Y=394
+CAM_ACCEPT_X=747
+CAM_ACCEPT_Y=217
+GPU_RUN_X=1860
+GPU_RUN_Y=315
+elif [[ "$CURRENT_W" -eq 1920 ]] && [[ "$CURRENT_H" -eq 1080 ]];then
+SWIPE_X1=900
+SWIPE_X2=544
+SWIPE_Y1=900
+SWIPE_Y2=0
+BT_TAP_X=900
+BT_TAP_Y=342
+CAM_CAPTURE_X=1770
+CAM_CAPTURE_Y=504
+CAM_BACK_X=1766
+CAM_BACK_Y=970
+CAM_ACCEPT_X=1768
+CAM_ACCEPT_Y=520
+GPU_RUN_X=1860
+GPU_RUN_Y=315
+elif [[ "$CURRENT_W" -eq 1280 ]] && [[ "$CURRENT_H" -eq 720 ]];then
+SWIPE_X1=600
+SWIPE_X2=362
+SWIPE_Y1=600
+SWIPE_Y2=0
+BT_TAP_X=600
+BT_TAP_Y=228
+CAM_CAPTURE_X=1180
+CAM_CAPTURE_Y=336
+CAM_BACK_X=1177
+CAM_BACK_Y=646
+CAM_ACCEPT_X=1178
+CAM_ACCEPT_Y=346
+GPU_RUN_X=660
+GPU_RUN_Y=100
+fi
+
+LEVEL=normal
+
+adb root
+sleep 3
+
+#unlock screen
+adb shell input touchscreen swipe "$SWIPE_X1" "$SWIPE_X2" "$SWIPE_Y1" "$SWIPE_Y2"
+
+
+# WIFI test
+adb shell "svc wifi enable"
+
+
+# Bluetooth test
+adb shell am start -a android.settings.BLUETOOTH_SETTINGS
+adb shell input tap "$BT_TAP_X" "$BT_TAP_Y"
+sleep 5
+adb exec-out screencap -p > test_bt_scan.png
+
+# Video Test
+adb shell cp -rv /mnt/media_rw/F556-9BAE/test_video/bbb_full.ffmpeg.1920x1080.mp4.libx265_6500kbps_30fps.libfaac_stereo_128kbps_48000Hz.mp4 /sdcard/Download/
+sleep 30
+adb shell am start -a android.intent.action.VIEW -d "file:///sdcard/Download/bbb_full.ffmpeg.1920x1080.mp4.libx265_6500kbps_30fps.libfaac_stereo_128kbps_48000Hz.mp4" -t "video/*"
+sleep 10
+adb exec-out screencap -p > test_vpu.png
+sleep 600
+
+# GPU test
+adb install ./gpu-stress.apk
+adb shell am start -n com.android.gpu_emulation_stress_test/.MainActivity
+sleep 5
+adb shell input tap "$GPU_RUN_X" "$GPU_RUN_Y"
+sleep 30
+adb exec-out screencap -p > test_gpu.png
+sleep 120
+
+
+# Camera test
+adb shell "am start -a android.media.action.IMAGE_CAPTURE" && sleep 1
+
+for i in {1..100}
+do
+  sleep 5
+  adb shell input tap "$CAM_CAPTURE_X" "$CAM_CAPTURE_Y"
+  sleep 1
+  adb exec-out screencap -p > camera-capture_test-"$i".png
+  if [ "$i" -eq 10 ];then
+    adb shell input tap "$CAM_ACCEPT_X" "$CAM_ACCEPT_Y"
+  else
+    adb shell input tap "$CAM_BACK_X" "$CAM_BACK_Y"
+  fi
+done
+
+# Web Browser test (queen's music video from youtube)
+# Ethernet first
+adb shell am start -a android.intent.action.VIEW -d https://www.youtube.com/watch?v=fJ9rUzIMcZQ
+sleep 40
+adb exec-out screencap -p > youtube_test_eth.png
+sleep 600
+
+# WiFi Second
+adb shell "ifconfig eth0 down"
+adb shell am start -a android.intent.action.VIEW -d https://www.youtube.com/watch?v=fJ9rUzIMcZQ
+sleep 40
+adb exec-out screencap -p > youtube_test_wifi.png
